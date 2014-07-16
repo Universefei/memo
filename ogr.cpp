e98e348a0516bb321f2ff6adab12eba1f709b2af
univfei <826171850@qq.com>
2014-07-16 18:44:45 +0800

    modify ogr.cpp

--- ogr.cpp
+++ ogr.cpp
@@ -1,28 +1,62 @@
 #include "gdal/ogrsf_frmts.h"
-//#include "gdal/gdal.h"
-//#include "gdal/gdal_priv.h"
 
 int main()
 
 {
     /// Register all OGR drivers
     OGRRegisterAll();
+    printf("register complete\n");
 
     /// Get PostgreSQL driver throught Singleton Manager of OGRSFDriverRegistrar.
-    const char *pszDriverName = "PG";
+    const char *pszDriverName = "PostgreSQL";
     OGRSFDriver *poDriver;
     poDriver = OGRSFDriverRegistrar::GetRegistrar()->GetDriverByName(pszDriverName);
+    if(NULL == poDriver)
+    {
+        printf("Can not get driver \n");
+    }
+    else
+    {
+        printf("Get Driver succeesfully\n");
+    }
 
     /// Open PostgreSQL database and establish connection.
     OGRDataSource *poDS;
-    poDS = poDriver->Open("host=192.168.1.99 port=5432 user=postgres password=postgres dbname=opengeo");
-
+    const char *pszConnInfo = "PG: host='192.168.1.99' port='5432' user='postgres' password='postgres' dbname='opengeo'";
+    poDS = poDriver->Open(pszConnInfo);
+    if(NULL == poDS)
+    {
+        printf("Can not connect to database\n");
+    }
+    else
+    {
+        printf("Connection established\n");
+    }
 
     /// Get Layer from DataSource.
     OGRLayer  *poLayer;
-    poLayer = poDS->GetLayerByName( "point" );
+    //poLayer = poDS->GetLayerByName( "waterply_900913" );
+    const char *pszSQLCommand = "SELECT * \
+                                  FROM waterply_900913 \
+                                  WHERE ST_Contains( ST_MakeEnvelope(-8584936,4691869,-8561767,4710000),geom );";
+    poLayer = poDS->ExecuteSQL(pszSQLCommand, NULL, NULL);
+    if(NULL == poLayer)
+    {
+        printf("Get no Result Layers failed\n");
+    }
+    else
+    {
+        printf("Get Result Layer succussful\n");
+    }
 
     /// Set Query statement here in OGRResultLayer.
+
+    /// Get Layer metadata.
+    OGRFeatureDefn *poFDefn = poLayer->GetLayerDefn();
+    if(NULL != poFDefn)
+    {
+        printf("Get Layer Feature definition successfully\n");
+    }
 
     /// Get Feature from Layer.
     OGRFeature *poFeature;
@@ -30,10 +64,9 @@ int main()
     while( (poFeature = poLayer->GetNextFeature()) != NULL )
     {
         /// Get Attribute fields.
-        OGRFeatureDefn *poFDefn = poLayer->GetLayerDefn();
         int iField;
-
-        for( iField = 0; iField < poFDefn->GetFieldCount(); iField++ )
+        int iFieldAmount = poFDefn->GetFieldCount();
+        for( iField = 0; iField < iFieldAmount; iField++ )
         {
             OGRFieldDefn *poFieldDefn = poFDefn->GetFieldDefn( iField );
 
